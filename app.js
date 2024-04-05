0abec06f3a84fa6e573ce17b4064a91043e00888
Hristina20 <165832473+Hristina20@users.noreply.github.com>
2024-04-05 08:42:56 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+function Add(num1, num2) {
+    return num1 + num2;
+};
+var resultAdd = Add (2, 3);
+console.log(resultAdd);
+
+function Subtract(num1, num2) {
+    return num1 - num2;
+}
+var resultSubtract = Subtract(10, 2)
+console.log(resultSubtract);
+
+function Multiply(num1, num2){
+    return num1 * num2
+};
+var resultMultiply = Multiply(2, 6);
+console.log(resultMultiply);
+
+function Divide(num1, num2) {
+    return num1 / num2;
+}
+var resultDivide = Divide(14, 2);
+console.log(resultDivide);
