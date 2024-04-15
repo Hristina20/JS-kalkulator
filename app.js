86640d7a8f292f5ae4196fdf5614063098729ce5
Hristina20 <165832473+Hristina20@users.noreply.github.com>
2024-04-15 11:06:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function Add(num1, num2) {
+/*function Add(num1, num2) {
     return num1 + num2;
 };
 var resultAdd = Add (2, 3);
@@ -20,4 +20,37 @@ function Divide(num1, num2) {
     return num1 / num2;
 }
 var resultDivide = Divide(14, 2);
-console.log(resultDivide);
+console.log(resultDivide);*/
+
+let currentResult = 0;
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", sub);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+
+function add() {
+    let calculationDescription = currentResult + " + " + userInput.value;
+    currentResult = currentResult + parseInt(userInput.value);
+    outputResult(currentResult, calculationDescription);
+}
+
+function sub() {
+    let calculationDescription = currentResult + " - " + userInput.value;
+    currentResult = currentResult - parseInt(userInput.value);
+    outputResult(currentResult, calculationDescription);
+}
+
+function multiply() {
+    let calculationDescription = currentResult + " * " + userInput.value;
+    currentResult = currentResult * parseInt(userInput.value);
+    outputResult(currentResult, calculationDescription);
+}
+
+function divide() {
+    let calculationDescription = currentResult + " / " + userInput.value;
+    currentResult = currentResult / parseInt(userInput.value);
+    outputResult(currentResult, calculationDescription);
+}
+
+
